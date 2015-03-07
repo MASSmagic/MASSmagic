5c390c76691f01974beee4ecc2d39285b25a323e
ijd2104 <ijd2104@columbia.edu>
2015-03-07 12:24:39 -0500

    changed api to avoid https

--- getISScoord.m
+++ getISScoord.m
@@ -1,14 +1,27 @@
-function [lat,lon] = getISScoord()
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-latlong=urlread('https://api.wheretheiss.at/v1/satellites/25544');
-latarr = strsplit(latlong,'"latitude":');
-latarr2 = strsplit(char(latarr(2)),',');
-lat = str2double(latarr2(1));
+function [ latitude, longitude ] = getISScoord()
+    %This function outputs the latitude and longitude of the current ISS
+    %position, if you were to project its location onto the surface of
+    %the earth. The method "urlread" sends a request to the Open Notify API,
+    %and recieves a JSON string response. The string is parsed so that only the
+    %desired information remains. Try unsuppressing (removing the semi-colons)
+    %after each line to see what is happening here. 
+    %Use "format long" in the command window to obtain more decimal precision. 
 
-longarr = strsplit(latlong,'"longitude":');
-longarr2 = strsplit(char(longarr(2)),',');
-lon = str2double(longarr2(1));
+    latlong=urlread('http://api.open-notify.org/iss-now.json');
+    latarr = strsplit(latlong,'"latitude":');
+    latarr2 = strsplit(char(latarr(2)),',');
+    latitude = str2double(latarr2(1));
+
+    longarr = strsplit(latlong,{'"longitude":','}'});
+    longitude = str2double(longarr(2));
+
+    if isnan(latitude)
+        longarr = strsplit(latlong,'"longitude":');
+        longarr2 = strsplit(char(longarr(2)),',');
+        longitude = str2double(longarr2(1));
+
+        latarr = strsplit(latlong,{'"latitude":','}'});
+        latitude = str2double(latarr(2));
+    end
 
 end
-
